1042f28e0221ad68e657c3060302a32478c0efa9
Lucas-ohlin <Lucas.persson.ohlin@hotmail.com>
2022-10-04 09:35:39 +0200

    Add project files.

---
+++ BankingHub/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BankingHub {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
