db5d87f5cdd1858b8fd5cf5d581e8ee9a9faa8e8
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2022-10-13 00:38:28 +0200

    Reworked the entire account dictionary and some other smaller tweaks. One person can have multiple accounts.

--- BankingHub/Program.cs
+++ BankingHub/Program.cs
@@ -1,126 +1,184 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace BankingHub {
 
     internal class Program {
 
-        //Dictionary with the accounts
-        public static Dictionary<int, List<string>> userAccounts = new Dictionary<int, List<string>>();
-        //Pins to use by the accounts and the fund dictionary
-        public static int[] pinArray = new int[5] { 123, 222, 333, 444, 555 };
-        //Dictionary with funds linked to accounts using pinArray
-        public static Dictionary<int, int> fundsDict = new Dictionary<int, int>();
+        //Account Dictionary
+        public static Dictionary<int, Dictionary<int, List<string>>> userAccounts = new Dictionary<int, Dictionary<int, List<string>>>();
+        //Availble Account Pins
+        public static int[] pinArray = new int[5] { 111, 222, 333, 444, 555 };
 
 
-        static void Main(string[] args) {
+        private static void Main(string[] args) {
+
             //Creates the 5 initial users
             UserCreation();
-               
+
+            //Calls the login method
+            LogIn();
+
+        }
+
+
+        private static void LogIn() {
+
             Console.WriteLine("Välkommen till Trönninge Bank!\n\nVänligen ange bank pin:");
 
             byte tries = 0;
             do {
-                int userPin = int.Parse(Console.ReadLine());
+
+                int inputPin = int.Parse(Console.ReadLine());
 
                 //Checks if the dictionary contains the key
-                if (userAccounts.ContainsKey(userPin)) {
-                    NavMenu(userPin);
+                if (userAccounts.ContainsKey(inputPin)) {
+                    NavMenu(inputPin);
                     return;
                 }
-                if (!userAccounts.ContainsKey(userPin)) {
-                    Console.WriteLine("Tyvärr inte ett korrekt pin, försök igen...");
+
+                if (!userAccounts.ContainsKey(inputPin)) {
+                    Console.WriteLine("\nTyvärr inte ett korrekt pin, försök igen...");
                     tries++;
                 }
 
             } while (tries < 3);
 
             Console.WriteLine("Tyvärr dina tre försök är upp...");
+
         }
 
 
         private static void NavMenu(int pin) {
-            //userAccounts[pin][0] = name
-            Console.WriteLine($"\nLoggade in till kontot: {userAccounts[pin][0]}");
+
+            //Prints out the logged in account name
+            Console.WriteLine($"\nLoggade in till kontot: {pin}");
 
             bool run = true;
-            while (run) {
-                //Prints out the choices
+            do {
+
                 Console.WriteLine("\n1. Se dina konton och saldo\r\n2. Överföring mellan konton\r\n3. Ta ut pengar\r\n4. Logga ut");
                 byte userChoice = byte.Parse(Console.ReadLine());
 
                 switch (userChoice) {
-                    //See all accounts
-                    case 1:
-                        Console.WriteLine("\nAlla nuvarande konton:");
-                        UserShowcase();
+                    default: //If not 1,2,3 or 4
+                        Console.WriteLine("Ogiltigt val.");
                         break;
-                    //Transfer funds between accounts
-                    case 2:
+
+                    case 1: //See all accounts
+                        UserShowcase(pin);
+                        EnterBack();
                         break;
-                    //Withdraw funds from account
-                    case 3:
-                        //pin is logged into account
-                        WithdrawFunds(pin);
+
+                    case 2: //Transfer funds between accounts
+                        EnterBack();
                         break;
-                    //Log out
-                    case 4:
-                        Console.WriteLine("\nLoggar ut...");
-                        return;
+
+                    case 3: //Withdraw funds from account
+                        WithdrawFunds(pin); //Pin for which account to log into
+                        EnterBack();
+                        break;
+
+                    case 4: //Log out
+                        Console.WriteLine($"\nLoggar ut ur {userAccounts[pin][0]}");
+                        run = false;
+                        LogIn();
+                        break;
                 }
-            }
+
+            } while (run);
 
         }
         
 
         private static void WithdrawFunds(int pin) {
-            //userAccounts[pin][0] = name | userAccounts[pin][1] = bal
-            Console.WriteLine($"\nFinns {userAccounts[pin][1]}kr på kontot: {userAccounts[pin][0]}\nHur mycket ska dras:");
+
+            UserShowcase(pin);
+
+            Console.WriteLine("\nVälj konto att dra ifrån:");
+            int accountChoice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nFinns {userAccounts[pin][accountChoice][1]}kr på kontot: {userAccounts[pin][accountChoice][0]} \nHur mycket ska dras:");
 
             while (true) {
-                int amount = int.Parse(Console.ReadLine());
+
+                float amount;
+                if (!float.TryParse(Console.ReadLine(), out amount))
+                    Console.WriteLine("\nInte ett korrekt nummer.");
 
                 //If the given amount is bigger than the funds
-                if (fundsDict[pin] < amount) {
+                if (float.Parse(userAccounts[pin][accountChoice][1]) < amount) {
                     Console.WriteLine("Kontot har inte tillräckligt med pengar.");
                 } 
                 else {
-                    //Parses the userAccounts[pin][1] string to int and subtracts amount from it and parse it back to string
-                    userAccounts[pin][1] = (int.Parse(userAccounts[pin][1]) - amount).ToString();
-                    Console.WriteLine($"Tog ut {amount}kr från {userAccounts[pin][0]}\nFinns nu {userAccounts[pin][1]}kr kvar på kontot.");
+                    //Parses the userAccounts[pin][accountChoice][1] string to int and subtracts amount from it and parse it back to string
+                    userAccounts[pin][accountChoice][1] = (float.Parse(userAccounts[pin][accountChoice][1]) - amount).ToString();
+                    Console.WriteLine($"\nTog ut {amount}kr från {userAccounts[pin][accountChoice][0]}\nFinns nu {userAccounts[pin][accountChoice][1]}kr kvar på kontot.");
 
-                    return;
+                    break;
                 }
+
             }
 
         }
 
 
-        private static void UserShowcase() {
-            //Gets all users in userAccounts dictionary, displays key, name and balance
-            foreach (var user in userAccounts)
-                Console.WriteLine($" Key: {user.Key}, Name: {user.Value[0]}, Balance: {int.Parse(user.Value[1])}");
+        private static void UserShowcase(int pin) {
+
+            Console.WriteLine($"\nDina nuvarande konton i {pin}:");
+
+            //Gets all dictionaries currently in the given dictionary, displays key, name of the account and the balance on t
+            foreach (var user in userAccounts[pin])
+                Console.WriteLine($" Key: {user.Key}, Name: {user.Value[0]}, {user.Value[1]} ");
+
+        }
+
+        
+        private static void EnterBack() {
+
+            Console.WriteLine("\nKlicka enter för att komma till huvudmenyn.");
+
+            //If the user presses Enter, return from this method
+            while (true) {
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    return;
+            }
+
         }
 
 
         private static void UserCreation() {
-            //Default funds in each pin
-            fundsDict.Add(pinArray[0], 1000);
-            fundsDict.Add(pinArray[1], 2000);
-            fundsDict.Add(pinArray[2], 3000);
-            fundsDict.Add(pinArray[3], 100);
-            fundsDict.Add(pinArray[4], 10);
 
-            //Key(Pin), { Full Name, Balance from funds dict }
-            userAccounts.Add(pinArray[0], new List<string>() { "Lucas Öhlin"    , fundsDict[pinArray[0]].ToString() });
-            userAccounts.Add(pinArray[1], new List<string>() { "Kalle Banan"    , fundsDict[pinArray[1]].ToString() });
-            userAccounts.Add(pinArray[2], new List<string>() { "Bob Bobson"     , fundsDict[pinArray[2]].ToString() });
-            userAccounts.Add(pinArray[3], new List<string>() { "Kungen"         , fundsDict[pinArray[3]].ToString() });
-            userAccounts.Add(pinArray[4], new List<string>() { "Leffe Attans"   , fundsDict[pinArray[4]].ToString() });
+            //Key(Pin), { Key(num) { Account, Balance} }
+            userAccounts.Add(pinArray[0], new Dictionary<int, List<string>> {
+                {1, new List<string>() { "Lönekonto" , 1000.0f.ToString() } },
+                {2, new List<string>() { "Sparkonto" , 1000.0f.ToString() } },
+            });
+
+            userAccounts.Add(pinArray[1], new Dictionary<int, List<string>> {
+                {1, new List<string>() { "Lönekonto" , 2000.0f.ToString() } },
+                {2, new List<string>() { "Sparkonto" , 2000.0f.ToString() } },
+            });
+
+            userAccounts.Add(pinArray[2], new Dictionary<int, List<string>> {
+                {1, new List<string>() { "Lönekonto" , 3000.0f.ToString() } },
+                {2, new List<string>() { "Sparkonto" , 3000.0f.ToString() } },
+            });
+
+            userAccounts.Add(pinArray[3], new Dictionary<int, List<string>> {
+                {1, new List<string>() { "Lönekonto" , 4000.0f.ToString() } },
+                {2, new List<string>() { "Sparkonto" , 4000.0f.ToString() } },
+            });
+
+            userAccounts.Add(pinArray[4], new Dictionary<int, List<string>> {
+                {1, new List<string>() { "Lönekonto" , 5000.0f.ToString() } },
+                {2, new List<string>() { "Sparkonto" , 5000.0f.ToString() } },
+            });
+
         }
 
 
     }
-
 
 }
