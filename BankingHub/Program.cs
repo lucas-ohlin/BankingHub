60d6f3230eb459512416a7d6952127b5b6ee20b8
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2022-10-14 01:19:13 +0200

    Everything done (comments, error handling etc) for every method other than move funds between accounts.

--- BankingHub/Program.cs
+++ BankingHub/Program.cs
@@ -37,7 +37,6 @@ namespace BankingHub {
                     NavMenu(inputPin);
                     return;
                 }
-
                 if (!userAccounts.ContainsKey(inputPin)) {
                     Console.WriteLine("\nTyvärr inte ett korrekt pin, försök igen...");
                     tries++;
@@ -65,23 +64,19 @@ namespace BankingHub {
                     default: //If not 1,2,3 or 4
                         Console.WriteLine("Ogiltigt val.");
                         break;
-
                     case 1: //See all accounts
                         UserShowcase(pin);
                         EnterBack();
                         break;
-
                     case 2: //Transfer funds between accounts
                         EnterBack();
                         break;
-
                     case 3: //Withdraw funds from account
                         WithdrawFunds(pin); //Pin for which account to log into
                         EnterBack();
                         break;
-
                     case 4: //Log out
-                        Console.WriteLine($"\nLoggar ut ur {userAccounts[pin][0]}");
+                        Console.WriteLine($"\nLoggar ut ur {pin}");
                         run = false;
                         LogIn();
                         break;
@@ -94,29 +89,73 @@ namespace BankingHub {
 
         private static void WithdrawFunds(int pin) {
 
+            //Displays all accounts in user
             UserShowcase(pin);
-
             Console.WriteLine("\nVälj konto att dra ifrån:");
-            int accountChoice = int.Parse(Console.ReadLine());
 
+            //Will continue to run until the user inputs either 1 or 2 (keys for the users accounts)
+            byte accountChoice;
+            while (true) {
+
+                //Error handling, will not crash if a char is inputed
+                if (!byte.TryParse(Console.ReadLine(), out accountChoice))
+                    Console.WriteLine("\nNummer endast.");
+
+                //If input is a valid key
+                if (accountChoice == 1 || accountChoice == 2)
+                    break;
+
+                else
+                    Console.WriteLine("Inte en valid key.\nFörsök igen:");
+            }
+
+            //Layout: users pin, which account in user and what part of the list to display
             Console.WriteLine($"\nFinns {userAccounts[pin][accountChoice][1]}kr på kontot: {userAccounts[pin][accountChoice][0]} \nHur mycket ska dras:");
 
             while (true) {
 
+                //Error handling
                 float amount;
                 if (!float.TryParse(Console.ReadLine(), out amount))
                     Console.WriteLine("\nInte ett korrekt nummer.");
 
-                //If the given amount is bigger than the funds
-                if (float.Parse(userAccounts[pin][accountChoice][1]) < amount) {
-                    Console.WriteLine("Kontot har inte tillräckligt med pengar.");
-                } 
-                else {
-                    //Parses the userAccounts[pin][accountChoice][1] string to int and subtracts amount from it and parse it back to string
-                    userAccounts[pin][accountChoice][1] = (float.Parse(userAccounts[pin][accountChoice][1]) - amount).ToString();
-                    Console.WriteLine($"\nTog ut {amount}kr från {userAccounts[pin][accountChoice][0]}\nFinns nu {userAccounts[pin][accountChoice][1]}kr kvar på kontot.");
+                //If the given amount is bigger than the funds availible
+                if (float.Parse(userAccounts[pin][accountChoice][1]) < amount) 
+                    Console.WriteLine($"\nKontot har inte mer än {userAccounts[pin][accountChoice][1]}kr\nFörsök igen:");
 
-                    break;
+                else {
+
+                    int inputPin;
+                    byte tries = 0;
+
+                    Console.WriteLine("\nAnge user pin för att dra ut pengar:");
+
+                    do {
+                        //Error handling
+                        if (!int.TryParse(Console.ReadLine(), out inputPin)) {
+                            Console.WriteLine("\nInte ett korrekt nummer.");
+                        }
+                        if (inputPin != pin) {
+                            Console.WriteLine("Inte en valid key.\nFörsök igen:");
+                            tries++;
+                        }
+                        if (inputPin == pin) {
+                            //Parses the userAccounts[pin][accountChoice][1] (user bal)
+                            //string to int and subtracts amount (user input) from it and parse it back to string
+                            userAccounts[pin][accountChoice][1] = (float.Parse(userAccounts[pin][accountChoice][1]) - amount).ToString();
+                            Console.WriteLine($"\nTog ut {amount}kr från {userAccounts[pin][accountChoice][0]}\n" +
+                                              $"Finns nu {userAccounts[pin][accountChoice][1]}kr kvar på kontot.");
+                            return;
+                        }
+
+                    } while (tries < 5);
+
+                    //If the given five tries are up user is sent back to the nav menu.
+                    if (tries == 5) {
+                        Console.WriteLine("Tyvärr dina fem försök är upp...");
+                        return;
+                    }
+
                 }
 
             }
@@ -125,32 +164,27 @@ namespace BankingHub {
 
 
         private static void UserShowcase(int pin) {
-
             Console.WriteLine($"\nDina nuvarande konton i {pin}:");
 
-            //Gets all dictionaries currently in the given dictionary, displays key, name of the account and the balance on t
+            //Gets all dictionaries currently in the user dictionary, displays key, name of the account and the balance
             foreach (var user in userAccounts[pin])
                 Console.WriteLine($" Key: {user.Key}, Name: {user.Value[0]}, {user.Value[1]} ");
-
         }
 
         
         private static void EnterBack() {
-
             Console.WriteLine("\nKlicka enter för att komma till huvudmenyn.");
 
             //If the user presses Enter, return from this method
-            while (true) {
+            while (true) 
                 if (Console.ReadKey().Key == ConsoleKey.Enter)
                     return;
-            }
-
         }
 
 
         private static void UserCreation() {
 
-            //Key(Pin), { Key(num) { Account, Balance} }
+            //User(Pin(From pinArray)), { Account(Pin) { Name, Balance} }
             userAccounts.Add(pinArray[0], new Dictionary<int, List<string>> {
                 {1, new List<string>() { "Lönekonto" , 1000.0f.ToString() } },
                 {2, new List<string>() { "Sparkonto" , 1000.0f.ToString() } },
